05540c4d633f08729928dbc90119358145a7af62
André Vonthron <andre.vonthron@vsk-software.com>
2026-09-25 11:40:20 +0200

    removed all api related stuff

--- src/de/rub/bi/inf/openbimrl/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.3.2 generiert 
-// Siehe <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
-// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2023.07.18 um 03:38:28 PM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://inf.bi.rub.de/OpenBimRL", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package de.rub.bi.inf.openbimrl;
